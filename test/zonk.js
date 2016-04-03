6ea412c79bfe5c9dedca4037371df8a804d06275
Māris Vilks <kaaposc@gmail.com>
2016-04-03 21:46:08 +0300

    Ones done

--- test/zonk.js
+++ test/zonk.js
@@ -90,10 +90,39 @@ describe('Game', function() {
 
   describe('rules say', function() {
     describe('keeping "1"', function() {
+      beforeEach(function() {
+        game.new();
+        game.throw();
+      });
+
       it('gives 100 pts if one is kept', function() {
         game.dice[0] = 1;
         game.keep(0);
         game.score.should.equal(100);
+      });
+
+      it('gives 200 pts if two are kept', function() {
+        game.dice[0] = game.dice[1] = 1;
+        game.keep(0, 1);
+        game.score.should.equal(200);
+      });
+
+      it('gives 1000 pts if three are kept', function() {
+        game.dice[0] = game.dice[1] = game.dice[2] = 1;
+        game.keep(0, 1, 2);
+        game.score.should.equal(1000);
+      });
+
+      it('gives 1000+1000+... pts if more than 3 are kept', function() {
+        game.dice[0] = game.dice[1] = game.dice[2] = game.dice[3] = 1;
+        game.keep(0, 1, 2, 3);
+        game.score.should.equal(2000);
+        game.dice[4] = 1;
+        game.keep(4);
+        game.score.should.equal(3000);
+        game.dice[5] = 1;
+        game.keep(5);
+        game.score.should.equal(4000);
       });
     });
   });
